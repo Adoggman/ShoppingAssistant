e5ef5ddd0dce4f78e33a911d520eee47c5740403
Andrew Hanrahan <andrew.hanrahan@gmail.com>
2013-11-05 22:15:26 -0600

    Added Register functionality

--- src/com/example/heisenberg/RegisterActivity.java
+++ src/com/example/heisenberg/RegisterActivity.java
@@ -1,11 +1,39 @@
 package com.example.heisenberg;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class RegisterActivity extends Activity{
+	
+	// Progress Dialog
+    private ProgressDialog pDialog;
+ 
+    JSONParser jsonParser = new JSONParser();
+    EditText inputName;
+    EditText inputEmail;
+    EditText inputPassword;
+ 
+    // url to create new product
+    private static String url_register_user = Constants.url+"register_user.php";
+ 
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +51,89 @@ public class RegisterActivity extends Activity{
                 finish();
             }
         });
+        
+        inputName = (EditText) findViewById(R.id.reg_fullname);
+        inputEmail = (EditText) findViewById(R.id.reg_email);
+        inputPassword = (EditText) findViewById(R.id.reg_password);
+        
+        Button btnRegister = (Button) findViewById(R.id.btnRegister);
+        
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+        	@Override
+        	public void onClick(View view) {
+        		new RegisterUser().execute();
+        	}
+        });
+        
     }
+	
+	
+	/**
+	 * Backgroudn Async Task to Register User
+	 */
+	class RegisterUser extends AsyncTask<String, String, String> {
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(RegisterActivity.this);
+            pDialog.setMessage("Registering user..");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+        
+        /**
+         * Creating product
+         * */
+        protected String doInBackground(String... args) {
+            String name = inputName.getText().toString();
+            String email = inputEmail.getText().toString();
+            String password = inputPassword.getText().toString();
+ 
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("email", email));
+            params.add(new BasicNameValuePair("password", password));
+            params.add(new BasicNameValuePair("name", name));
+ 
+            // getting JSON Object
+            // Note that create product url accepts POST method
+            JSONObject json = jsonParser.makeHttpRequest(url_register_user,
+                    "POST", params);
+ 
+            // check log cat for response
+            Log.d("Create Response", json.toString());
+ 
+            // check for success tag
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+ 
+                if (success == 1) {
+                    // successfully created product
+                    Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(i);
+ 
+                    // closing this screen
+                    finish();
+                } else {
+                    // failed to create product
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+ 
+            return null;
+        }
+ 
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once done
+            pDialog.dismiss();
+        }
+	}
 }
